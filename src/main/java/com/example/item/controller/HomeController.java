0efd86af70a44cc73a629ab36ca93e2e29c636ed
snehajais112 <snehajais2021@gmail.com>
2026-02-06 14:33:55 +0530

    Added home and health endpoints

---
+++ src/main/java/com/example/item/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.item.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+    @RestController
+    public class HomeController {
+
+        @GetMapping("/")
+        public String home() {
+            return "Spring Boot Item API is running successfully on Koyeb!";
+        }
+
+        @GetMapping("/health")
+        public String health() {
+            return "Server is running fine!";
+        }
+    }
+
+
